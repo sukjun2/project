d63f2204e81f145472fb13bd0b60f658f3b2defd
sukjun2 <tjrwns3650@gmail.com>
2022-10-27 17:49:20 +0900

    first commit

---
+++ php/mypage/mypageModify.php
@@ -0,0 +1,107 @@
+<?php
+
+include "../connect/connect.php";
+include "../connect/session.php";
+    
+    $type = $_POST['type'];
+    $userMemberID = $_SESSION['userMemberID'];
+
+    
+    if($type == "nicknameChange"){
+        $nickname = $_POST['nickname'];
+        $sql = "UPDATE userMember SET userNickName = '$nickname' WHERE userMemberID = '$userMemberID'";
+        $result = $connect -> query($sql);
+
+        echo json_encode(array("nickname" => $nickname));
+    }
+
+    if($type == "gender"){
+        $gender = $_POST['gender'];
+        $sql = "UPDATE userMember SET userGender = '$gender' WHERE userMemberID = '$userMemberID'";
+        $result = $connect -> query($sql);
+
+        echo json_encode(array("gender" => $gender));
+    }
+
+    if($type == "phonenumber"){
+        $phonenumber = $_POST['phonenumber'];
+        $sql = "UPDATE userMember SET userPhone = '$phonenumber' WHERE userMemberID = '$userMemberID'";
+        $result = $connect -> query($sql);
+
+        echo json_encode(array("phonenumber" => $phonenumber));
+    }
+
+    if($type == "userShortInfo"){
+        $userShortInfo = $_POST['userShortInfo'];
+        $sql = "UPDATE userMember SET userShortInfo = '$userShortInfo' WHERE userMemberID = '$userMemberID'";
+        $result = $connect -> query($sql);
+
+        echo json_encode(array("userShortInfo" => $userShortInfo));
+    }
+
+    if($type == "userLongInfo"){
+        $userLongInfo = $_POST['userLongInfo'];
+        $sql = "UPDATE userMember SET userLongInfo = '$userLongInfo' WHERE userMemberID = '$userMemberID'";
+        $result = $connect -> query($sql);
+
+        echo json_encode(array("userLongInfo" => $sql));
+    }
+
+    if($type == "userOneInfo"){
+        $userOneInfo = $_POST['userOneInfo'];
+        $sql = "UPDATE userMember SET userOneInfo = '$userOneInfo' WHERE userMemberID = '$userMemberID'";
+        $result = $connect -> query($sql);
+
+        echo json_encode(array("userOneInfo" => $sql));
+    }
+
+    if($type == "joinLeave"){
+        $youPass = $_POST['youPass'];
+        $youPass = sha1("web".$youPass);
+        $sql2 = "SELECT * FROM userMember WHERE userMemberID = $userMemberID AND userPass = '$youPass'";
+        $result2 = $connect -> query($sql2);
+
+        if($result2 -> num_rows == 1){
+            $sql = "DELETE FROM userMember WHERE userMemberID = $userMemberID AND userPass = '$youPass'";
+            $result = $connect -> query($sql);
+            unset($_SESSION['userMemberID']);
+            unset($_SESSION['userId']);
+            unset($_SESSION['userName']);
+            $jsonResult = "good";
+        } else {
+            $jsonResult = "bad";
+        }
+        echo json_encode(array("result" => $jsonResult));
+    }
+
+    if($type == "categoryscroll"){
+        $page = $_POST['page'];
+        $viewNum = 8;
+        $viewLimit = ($viewNum * $page) - $viewNum;
+        $tagfile = '';
+        $categoryCountSql = "SELECT count(categgoryBoardID) as cate FROM categoryBoard as b JOIN userMember as m ON b.userMemberID = m.userMemberID  WHERE m.userMemberID = '{$userMemberID}' ";
+        $categoryCountResult = $connect -> query($categoryCountSql);
+
+        $categoryCount = $categoryCountResult -> fetch_array(MYSQLI_ASSOC);
+        $categoryCount = $categoryCount['cate'];
+        $categoryCount = ceil($categoryCount/$viewNum);
+
+        if($page <= $categoryCount){
+            $boardSql = "SELECT * FROM categoryBoard as b JOIN userMember as m ON b.userMemberID = m.userMemberID  WHERE m.userMemberID = '{$userMemberID}' ORDER BY b.categgoryBoardID DESC LIMIT {$viewLimit}, {$viewNum} ";
+            $boardResult = $connect -> query($boardSql);
+            foreach($boardResult as $board) {    
+                $tagfile .= "<article class='mypage__cardBox'>
+                        <div class='cardBox__image'>
+                            <figure>
+                                <a href='#'><img src='../assets/categoryImg/".$board['categgoryPhoto']."' alt='이미지' /></a>
+                            </figure>
+                        </div>
+                    </article>";
+                } 
+                echo json_encode(array("result" => 'good', "page" => $tagfile));
+        } else echo json_encode(array("result" => 'bad'));
+
+    }
+
+?>
+
---
+++ php/search/search.php
@@ -0,0 +1,330 @@
+<?php
+include "../connect/connect.php";
+include "../connect/session.php";
+$searchKeyword = $_GET['searchKeyword'];
+?>
+<!DOCTYPE html>
+<html lang="ko">
+    <head>
+        <meta charset="UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>IT.D | 무료 이미지 다운로드 사이트</title>
+        <!-- CSS 링크 -->
+        <link rel="stylesheet" href="../assets/css/common.css">
+        <link rel="stylesheet" href="../assets/css/reset.css">
+        <link rel="stylesheet" href="../assets/css/search.css">
+        <link rel="stylesheet" href="../assets/css/fonts.css">
+    </head>
+    <body>
+        <?php include "../include/header.php" ?>
+        <!-- header -->
+        
+        <section class="search_banner">
+            <div class="banner_inner container">
+                <p>SEARCH</p>
+                <div class="banner_udline"></div>
+            </div>
+        </section>
+
+        <section class="search_wrap ">
+            <div class="search_inner container">
+                <div class="search_input">
+                    <label for="search" class="ir"></label>
+                    <img src="../../assets/image/search_Icon.svg" alt="검색">
+                    <input onkeypress="show_name(event)" id="search__inputResult" name="search__inputResult" type="text" placeholder="검색어를 입력해주세요." value="<?=$searchKeyword?>" />
+                    <button id="search__input__searchBtn">검색</button>
+                </div>
+            </div>
+            <div class="search_sort container">
+                <select name="image" id="image_sort">
+                    <option value="Wallpaper">Wallpaper</option>
+                    <?php
+                    
+                    ?>
+                    <option value="Photo">Photo</option>
+                    <option value="Picture">Picture</option>
+                    <?php
+                    
+                    ?>
+                </select>
+                <select name="image" id="image_sort">
+                    <option value="date">날짜별</option>
+                    <option value="viewcount">조회수</option>
+                    <option value="like">좋아요수</option>
+                </select>
+                <div class="num">Total Found:00</div>
+            </div>
+        </section>
+
+
+        <main id="main">
+            <div class="main_wrap">
+                <div class="main_inner">
+                    <section class="main_card container">
+                        <article class="main_cardBox">
+                            <div class="main_image">
+                                <figure>
+                                    <a href="#"><img src="../../assets/image/main_image01.jpg" alt="이미지" /></a>
+                                </figure>
+                            </div>
+                            <div class="main_info">
+                                <div class="mainInfo_left">
+                                    <figure>
+                                        <a href="#"><img src="../../assets/image/profile_ico.png" alt="프로필 이미지" /></a>
+                                    </figure>
+                                    <div class="mainInfo_title">
+                                        <h3><a href="#">이미지 제목</a></h3>
+                                        <span>크리에이터 이름</span>
+                                    </div>
+                                </div>
+
+                                <div class="mainInfo_right">
+                                    <figure>
+                                        <img src="../assets/image/comment_ico.svg" alt="댓글뷰 이미지" />
+                                    </figure>
+                                    <span>20</span>
+                                    <figure>
+                                        <img src="../assets/image/view_ico.svg" alt="댓글뷰 이미지" />
+                                    </figure>
+                                    <span>12</span>
+                                </div>
+                            </div>
+                            <img src="../assets/image/heartBasic_ico.svg" alt="공감 아이콘" />
+                        </article>
+                        <article class="main_cardBox">
+                            <div class="main_image">
+                                <figure>
+                                    <a href="#"><img src="../assets/image/main_image02.jpg" alt="이미지" /></a>
+                                </figure>
+                            </div>
+                            <div class="main_info">
+                                <div class="mainInfo_left">
+                                    <figure>
+                                        <a href="#"><img src="../assets/image/profile_ico.png" alt="프로필 이미지" /></a>
+                                    </figure>
+                                    <div class="mainInfo_title">
+                                        <h3><a href="#">이미지 제목</a></h3>
+                                        <span>크리에이터 이름</span>
+                                    </div>
+                                </div>
+
+                                <div class="mainInfo_right">
+                                    <figure>
+                                        <img src="../assets/image/comment_ico.svg" alt="댓글뷰 이미지" />
+                                    </figure>
+                                    <span>20</span>
+                                    <figure>
+                                        <img src="/assets/image/view_ico.svg" alt="댓글뷰 이미지" />
+                                    </figure>
+                                    <span>12</span>
+                                </div>
+                            </div>
+                            <img src="/assets/image/heartBasic_ico.svg" alt="공감 아이콘" />
+                        </article>
+                        <article class="main_cardBox">
+                            <div class="main_image">
+                                <figure>
+                                    <a href="#"><img src="/assets/image/main_image03.jpg" alt="이미지" /></a>
+                                </figure>
+                            </div>
+                            <div class="main_info">
+                                <div class="mainInfo_left">
+                                    <figure>
+                                        <a href="#"><img src="/assets/image/profile_ico.png" alt="프로필 이미지" /></a>
+                                    </figure>
+                                    <div class="mainInfo_title">
+                                        <h3><a href="#">이미지 제목</a></h3>
+                                        <span>크리에이터 이름</span>
+                                    </div>
+                                </div>
+
+                                <div class="mainInfo_right">
+                                    <figure>
+                                        <img src="/assets/image/comment_ico.svg" alt="댓글뷰 이미지" />
+                                    </figure>
+                                    <span>20</span>
+                                    <figure>
+                                        <img src="/assets/image/view_ico.svg" alt="댓글뷰 이미지" />
+                                    </figure>
+                                    <span>12</span>
+                                </div>
+                            </div>
+                            <img src="/assets/image/heartBasic_ico.svg" alt="공감 아이콘" />
+                        </article>
+                        <article class="main_cardBox">
+                            <div class="main_image">
+                                <figure>
+                                    <a href="#"><img src="/assets/image/main_image04.jpg" alt="이미지" /></a>
+                                </figure>
+                            </div>
+                            <div class="main_info">
+                                <div class="mainInfo_left">
+                                    <figure>
+                                        <a href="#"><img src="/assets/image/profile_ico.png" alt="프로필 이미지" /></a>
+                                    </figure>
+                                    <div class="mainInfo_title">
+                                        <h3><a href="#">이미지 제목</a></h3>
+                                        <span>크리에이터 이름</span>
+                                    </div>
+                                </div>
+
+                                <div class="mainInfo_right">
+                                    <figure>
+                                        <img src="/assets/image/comment_ico.svg" alt="댓글뷰 이미지" />
+                                    </figure>
+                                    <span>20</span>
+                                    <figure>
+                                        <img src="/assets/image/view_ico.svg" alt="댓글뷰 이미지" />
+                                    </figure>
+                                    <span>12</span>
+                                </div>
+                            </div>
+                            <img src="/assets/image/heartBasic_ico.svg" alt="공감 아이콘" />
+                        </article>
+                        <article class="main_cardBox">
+                            <div class="main_image">
+                                <figure>
+                                    <a href="#"><img src="/assets/image/main_image05.jpg" alt="이미지" /></a>
+                                </figure>
+                            </div>
+                            <div class="main_info">
+                                <div class="mainInfo_left">
+                                    <figure>
+                                        <a href="#"><img src="/assets/image/profile_ico.png" alt="프로필 이미지" /></a>
+                                    </figure>
+                                    <div class="mainInfo_title">
+                                        <h3><a href="#">이미지 제목</a></h3>
+                                        <span>크리에이터 이름</span>
+                                    </div>
+                                </div>
+
+                                <div class="mainInfo_right">
+                                    <figure>
+                                        <img src="/assets/image/comment_ico.svg" alt="댓글뷰 이미지" />
+                                    </figure>
+                                    <span>20</span>
+                                    <figure>
+                                        <img src="/assets/image/view_ico.svg" alt="댓글뷰 이미지" />
+                                    </figure>
+                                    <span>12</span>
+                                </div>
+                            </div>
+                            <img src="/assets/image/heartBasic_ico.svg" alt="공감 아이콘" />
+                        </article>
+                        <article class="main_cardBox">
+                            <div class="main_image">
+                                <figure>
+                                    <a href="#"><img src="/assets/image/main_image06.jpg" alt="이미지" /></a>
+                                </figure>
+                            </div>
+                            <div class="main_info">
+                                <div class="mainInfo_left">
+                                    <figure>
+                                        <a href="#"><img src="/assets/image/profile_ico.png" alt="프로필 이미지" /></a>
+                                    </figure>
+                                    <div class="mainInfo_title">
+                                        <h3><a href="#">이미지 제목</a></h3>
+                                        <span>크리에이터 이름</span>
+                                    </div>
+                                </div>
+
+                                <div class="mainInfo_right">
+                                    <figure>
+                                        <img src="/assets/image/comment_ico.svg" alt="댓글뷰 이미지" />
+                                    </figure>
+                                    <span>20</span>
+                                    <figure>
+                                        <img src="/assets/image/view_ico.svg" alt="댓글뷰 이미지" />
+                                    </figure>
+                                    <span>12</span>
+                                </div>
+                            </div>
+                            <img src="/assets/image/heartBasic_ico.svg" alt="공감 아이콘" />
+                        </article>
+                        <article class="main_cardBox">
+                            <div class="main_image">
+                                <figure>
+                                    <a href="#"><img src="/assets/image/main_image07.jpg" alt="이미지" /></a>
+                                </figure>
+                            </div>
+                            <div class="main_info">
+                                <div class="mainInfo_left">
+                                    <figure>
+                                        <a href="#"><img src="/assets/image/profile_ico.png" alt="프로필 이미지" /></a>
+                                    </figure>
+                                    <div class="mainInfo_title">
+                                        <h3><a href="#">이미지 제목</a></h3>
+                                        <span>크리에이터 이름</span>
+                                    </div>
+                                </div>
+
+                                <div class="mainInfo_right">
+                                    <figure>
+                                        <img src="/assets/image/comment_ico.svg" alt="댓글뷰 이미지" />
+                                    </figure>
+                                    <span>20</span>
+                                    <figure>
+                                        <img src="/assets/image/view_ico.svg" alt="댓글뷰 이미지" />
+                                    </figure>
+                                    <span>12</span>
+                                </div>
+                            </div>
+                            <img src="/assets/image/heartBasic_ico.svg" alt="공감 아이콘" />
+                        </article>
+                        <article class="main_cardBox">
+                            <div class="main_image">
+                                <figure>
+                                    <a href="#"><img src="/assets/image/main_image08.jpg" alt="이미지" /></a>
+                                </figure>
+                            </div>
+                            <div class="main_info">
+                                <div class="mainInfo_left">
+                                    <figure>
+                                        <a href="#"><img src="/assets/image/profile_ico.png" alt="프로필 이미지" /></a>
+                                    </figure>
+                                    <div class="mainInfo_title">
+                                        <h3><a href="#">이미지 제목</a></h3>
+                                        <span>크리에이터 이름</span>
+                                    </div>
+                                </div>
+
+                                <div class="mainInfo_right">
+                                    <figure>
+                                        <img src="/assets/image/comment_ico.svg" alt="댓글뷰 이미지" />
+                                    </figure>
+                                    <span>20</span>
+                                    <figure>
+                                        <img src="/assets/image/view_ico.svg" alt="댓글뷰 이미지" />
+                                    </figure>
+                                    <span>12</span>
+                                </div>
+                            </div>
+                            <img src="/assets/image/heartBasic_ico.svg" alt="공감 아이콘" />
+                        </article>
+                    </section>
+                </div>
+            </div>
+        </main>
+        <!-- main -->
+        <?php include "../include/footer.php" ?>
+        <!-- // footer -->
+        <script>
+            document.querySelector(".header__bottom").style.display = "none";        
+        </script>
+        <script>
+            const searchBtn = document.querySelector('#search__input__searchBtn')
+            searchBtn.addEventListener('click', () =>{
+                console.log()
+                let resultText = document.querySelector('#search__inputResult').value
+                location.href=`search.php?searchKeyword=${resultText}`;
+            })
+            function show_name(e){
+                let resultText = document.querySelector('#search__inputResult').value
+                if(e.keyCode == 13){   
+                    location.href=`earch.php?searchKeyword=${resultText}`;
+                }
+            }
+        </script>
+    </body>
+</html>
