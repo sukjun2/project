a2ca9c00eb19982a4946ee1756db893fad2bb9f9
sukjun2 <tjrwns3650@gmail.com>
2022-11-03 17:02:03 +0900

    22.11.03

--- php/connect/connect.php
+++ php/connect/connect.php
@@ -5,9 +5,10 @@
     $db = "tjrwns3650";
     $connect = new mysqli($host, $user, $pass, $db);
     $connect -> set_charset("utf8");
-    if(mysqli_connect_errno()){
-        echo "Database Connect False";
-    } else {
-        echo "Database Connect True";
-    }
+
+    // if(mysqli_connect_errno()){
+    //     echo "Database Connect False";
+    // } else {
+    //     echo "Database Connect True";
+    // }
 ?>
